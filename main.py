8e15a9360b8051a3030d0ddbb8568364da70243f
nescapp <97590612+nescapp@users.noreply.github.com>
2023-11-14 22:34:47 +0100

    added get_role and debug

--- main.py
+++ main.py
@@ -17,11 +17,7 @@ def get_id() -> int:
     """
     Protocole MicroID™️ permettant d'attribuer un ID (1 ou 2) à chaque micro:bit
     """
-    display.show(Image("00000:"
-                    "00000:"
-                    "90909:"
-                    "00000:"
-                    "00000"))
+    display.show(Image("00000:""00000:""90909:""00000:""00000"))
     while True:
         radio.send("ID|1|1") # can i be mb1?
         sleep(random.randint(0, 100))
@@ -29,11 +25,13 @@ def get_id() -> int:
         if message:
             if message == "ID|1|1": #can i be mb1?
                 radio.send("ID|1|2") # yes you can, i'm mb2!
+                print("ID: 2")
                 return 2
             elif message == "ID|1|2": # yes you can, i'm mb2!
+                print("ID: 1")
                 return 1
 
-    # confirmation (A FAIRE)
+    # confirmation (A FAIRE) parce que oui, il existen encore un micro possibilité que les deux micro:bit envoient en même temps le message "ID|1|1" 
 
 def check_connection() -> bool:
     """
@@ -42,4 +40,29 @@ def check_connection() -> bool:
     # A FAIRE
     pass
 
+def get_role() -> str:
+    display.show("?")
+    while True:
+        message = radio.receive()
+        if button_a.is_pressed() or message == "ROLE|1|E":
+            radio.send("ROLE|1|P")
+            display.show("P")
+            sleep(1000)
+            display.clear()
+            print("Parent")
+            return "P"
+        elif button_b.is_pressed() or message == "ROLE|1|P":
+            radio.send("ROLE|1|E")
+            display.show("E")
+            sleep(1000)
+            display.clear()
+            print("Child")
+            return "E"
+            
+
 ID = get_id()
+# debug
+# display.show(str(ID))
+# sleep(3000)
+display.clear()
+ROLE = get_role()
