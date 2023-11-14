771b5d9f7c758714306debded80dbf079d265a48
nescapp <97590612+nescapp@users.noreply.github.com>
2023-11-14 22:13:51 +0100

    formating

--- main.py
+++ main.py
@@ -13,28 +13,33 @@ INITIAL_GROUP = 42
 radio.on()
 radio.config(channel=INITIAL_CHANNEL, group=INITIAL_GROUP)
 
-# Protocole MicroID™️
-display.show(Image("00000:"
-                   "00000:"
-                   "90909:"
-                   "00000:"
-                   "00000"))
-while True:
-    radio.send("can i be mb1?")
-    sleep(random.randint(0, 100))
-    message = radio.receive()
-    if message:
-        if message == "can i be mb1?":
-            ID =2
-            radio.send("yes you can, i'm mb2!")
-            # for x in range(10):
-            #     sleep(random.randint(0, 10))
-            #     radio.send("yes you can, i'm mb2!")
-            break
-        elif message == "yes you can, i'm mb2!":
-            ID = 1
-            break
+def get_id() -> int:
+    """
+    Protocole MicroID™️ permettant d'attribuer un ID (1 ou 2) à chaque micro:bit
+    """
+    display.show(Image("00000:"
+                    "00000:"
+                    "90909:"
+                    "00000:"
+                    "00000"))
+    while True:
+        radio.send("ID|1|1") # can i be mb1?
+        sleep(random.randint(0, 100))
+        message = radio.receive()
+        if message:
+            if message == "ID|1|1": #can i be mb1?
+                radio.send("ID|1|2") # yes you can, i'm mb2!
+                return 2
+            elif message == "ID|1|2": # yes you can, i'm mb2!
+                return 1
 
-# confirmation (A FAIRE)
+    # confirmation (A FAIRE)
 
-display.show(str(ID))
+def check_connection() -> bool:
+    """
+    Vérifie si la connexion est toujours active
+    """
+    # A FAIRE
+    pass
+
+ID = get_id()
