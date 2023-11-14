36198e631f8227b9b0acb6d660c6cc7e58fd29d2
nescapp <97590612+nescapp@users.noreply.github.com>
2023-11-14 22:03:03 +0100

    MicroID Implementation

--- main.py
+++ main.py
@@ -4,21 +4,37 @@ P3-SINF11BA1-A21
 
 from microbit import *
 import radio
+import random
 
 # initialisation
-
 INITIAL_CHANNEL = 69
 INITIAL_GROUP = 42
 
 radio.on()
 radio.config(channel=INITIAL_CHANNEL, group=INITIAL_GROUP)
 
+# Protocole MicroID™️
+display.show(Image("00000:"
+                   "00000:"
+                   "90909:"
+                   "00000:"
+                   "00000"))
 while True:
-    if button_a.is_pressed():
-        radio.send("1")
+    radio.send("can i be mb1?")
+    sleep(random.randint(0, 100))
     message = radio.receive()
-    if message == "1":
-        display.show(Image.HAPPY)
-        sleep(10)
-    else:
-        display.clear()
+    if message:
+        if message == "can i be mb1?":
+            ID =2
+            radio.send("yes you can, i'm mb2!")
+            # for x in range(10):
+            #     sleep(random.randint(0, 10))
+            #     radio.send("yes you can, i'm mb2!")
+            break
+        elif message == "yes you can, i'm mb2!":
+            ID = 1
+            break
+
+# confirmation (A FAIRE)
+
+display.show(str(ID))
