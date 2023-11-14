c8443eeb6fc86d7b4f9cc8dcdb2da90e56887ce7
nescapp <97590612+nescapp@users.noreply.github.com>
2023-11-14 17:37:35 +0100

    comm tests

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+"""
+P3-SINF11BA1-A21
+"""
+
 from microbit import *
-display.scroll("Hello, World!")
+import radio
+
+# initialisation
+
+INITIAL_CHANNEL = 69
+INITIAL_GROUP = 42
+
+radio.on()
+radio.config(channel=INITIAL_CHANNEL, group=INITIAL_GROUP)
+
+while True:
+    if button_a.is_pressed():
+        radio.send("1")
+    message = radio.receive()
+    if message == "1":
+        display.show(Image.HAPPY)
+        sleep(10)
+    else:
+        display.clear()
