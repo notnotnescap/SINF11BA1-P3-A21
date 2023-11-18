cc5ec5765d85765dc583128f62923dd4eb502bd6
nescapp <97590612+nescapp@users.noreply.github.com>
2023-11-19 00:02:06 +0100

    réorganisation du code et quelques avancements

--- README.md
+++ README.md
@@ -2,23 +2,62 @@
 
 Cette repo contient le code source du projet 3
 
-# Description du fonctionnement
+# Manuel d'utilisation de l'utilisateur
 
-Si `DEV_BYPASS_GET_ID` est `True` (Elle l'est par défaut) la m:b va bypass la fonction `get_id()` vous permettant de tester le code sans avoir à utiliser deux m:bs.
+Note : Si `DEV_BYPASS_GET_ID` est `True` (Elle l'est par défaut) la m:b va bypass la fonction `get_id()` vous permettant de tester le code sans avoir à utiliser deux m:bs.
 
 1. La m:b affiche "?", il faut alors appuier sur A pour le mode Parent ou B pour le mode Enfant. (Si une deuximème microbit est connectée, elle s'adaptera automatiquement)
 2. La m:b affiche "P" ou "E" selon le mode choisi pendent une seconde.
-3. Si vous avez choisi le mode Enfant, rien ne se passera vu qu'il n'est pas encore implémenté. 😂
-4. Si vous avez choisi le mode Parent, vous accederez au menu parent que vous naviguez avec A et B. Appuiez sur A et B en même temps pour valider votre choix. (Cependant, la m:b va juste afficher "?" car aucun mode n'est implémenté pour le moment)
-5. Vous pouvez alors appuier sur le logo microbit pour revenir au menu parent.
 
-Petit conseil : Si vous essayez de lire et comprendre le code, ne le lisez pas dans l'ordre mais plutôt en suivant le chemin de l'execution du code.
+## Mode Enfant
+Si vous avez choisi le mode Enfant, rien ne se passera vu qu'il n'est pas encore implémenté. 😂
 
-# Menu Parent 1 : Quantité de lait
+## Mode Parent
+Si vous avez choisi le mode Parent, vous accederez au menu parent que vous naviguez avec A et B. Appuiez sur A et B en même temps pour valider votre choix.
 
-Pour utiliser le menu:
+Vous pouvez alors appuier sur le logo microbit pour revenir au menu parent.
+
+### Mode 1 : Quantité de lait
 
 1. Appuyer sur les deux boutons (a et b) sur le microbit parents à l'affichage de la lettre L dans le menu global
 2. Pour ajouter une certaine quantité de lait, appuyez sur le boutons a. Chaque unité (un point lumineux), correspont à une valeur standart de 100mL de lait.
 3. Pour corriger et retirer une quantité de lait, appuyez sur le bouton b.
 4. Pour revenir en arrière il suffit d'appuyer sur le bouton tactile au-dessus du display
+
+### Mode 2 : Statut de l'enfant
+
+1. Appuyer sur les deux boutons (a et b) sur le microbit parents à l'affichage de la lettre S dans le menu global
+2. Une animation indique l'état de l'endant
+
+### Mode 3 : Température
+
+1. Appuyer sur les deux boutons (a et b) sur le microbit parents à l'affichage de la lettre T dans le menu global
+
+(pas encore implémenté)
+
+### Mode 4 : Recherche
+
+1. Appuyer sur les deux boutons (a et b) sur le microbit parents à l'affichage de la lettre R dans le menu global
+
+(pas encore implémenté)
+
+# Manuel développeur
+
+## Types de messages
+
+les messages sont formatés de manière T|L|C où T est le type de message, L est la longueur du message et C est le contenu du message. Voici une liste des types de messages :
+
+`IDa` *(ID ask)* - Demande d'ID\
+`IDc` *(ID confirm)* - Confirmation d'ID\
+`ROLEc` *(ROLE confirm)* - Confirmation de rôle\
+`STATUSa` *(STATUS ask)* - Demande de status\
+`STATUSr` *(STATUS response)* - Mise à jour sur le status de la m:b\
+`CMD` - Envoie d'une commande\
+
+Note : il faudrait peut-être repenser le format des messages pour qu'ils soient plus faciles à comprendre (c'est pas grave si ils sont plus longs)
+
+# Types de commendes
+
+Note : Les commendes vont exclusivement de la m:b parent à la m:b enfant
+
+`START_SEARCH` - Début de la recherche d'une m:b enfant\
--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 P3-SINF11BA1-A21
 """
 
-import random
 from microbit import *
 import radio
 
@@ -11,8 +10,9 @@ INITIAL_CHANNEL = 69
 INITIAL_GROUP = 42
 DEV_BYPASS_GET_ID = True # True pour faire des tests avec qu'une seule m:b
 MDP = "00900:00900:00900:00900:00900"
-IMAGE_SLEEP_SEQUENCE = [Image("00000:""00000:""99099:""00000:""09990"),Image("00000:""99990:""00900:""09000:""99990"), Image("09999:""00090:""00900:""09999:""00000")]
+# pour tout ce qui est image il faut les initialiser après le choix du role pour utiliser moins de mémoire
 
+# initialisation de la radio
 radio.on()
 radio.config(channel=INITIAL_CHANNEL, group=INITIAL_GROUP)
 
@@ -78,59 +78,6 @@ class ImageMdp():
                             self.StrucMdpActu = self.StrucMdpActu[:i-2] + "09" + "0" + self.StrucMdpActu[i+1:]
                         print(self.StrucMdpActu)
                         return True
-                    
-    
-
-        
-
-
-
-class ImageCompteurLait():
-    """Classe permettant de gérer une représentation visuelle de la quantité de lait sur l'écran de la m:b Parent"""
-    def __init__(self) -> None:
-        self.imageStruc = "00000:""00000:""00000:""00000:""00000"
-        self.image = Image(self.imageStruc)
-
-    def showImage(self):
-        """Retourne l'image de la quantité de lait"""
-        return Image(self.imageStruc)
-
-    def checkStruc(self):
-        """Retourne l'index du premier 0 dans la structure de l'image"""
-        n = 0
-        for i in self.imageStruc:
-            if str(i).isdigit():
-                if i == "0":
-                    return n
-            n+=1
-        return n
-    
-    def addPin(self) -> None:
-        """Ajoute un point à la représentation visuelle de la quantité de lait"""
-        n = self.checkStruc()
-        if n > 28:
-            pass
-        else:
-            print("OUI") # on peut retirer ?
-            self.imageStruc = self.imageStruc[:n] + "9" + self.imageStruc[n + 1:]
-        self.image = Image(self.imageStruc)
-        print("[INFO] STRUCTURE IMAGE", self.imageStruc)
-
-    def removePin(self) -> None:
-        """Retire un point à la représentation visuelle de la quantité de lait"""
-        n = self.checkStruc()
-        print(n)
-        if n > 28:
-            self.imageStruc = "99999:""99999:""99999:""99999:""99990"
-        elif n == 0:
-            pass
-        else:
-            if n in [6, 12, 18, 24]:
-                self.imageStruc = self.imageStruc[:n-2] + "0:" + self.imageStruc[n:]
-            else:
-                self.imageStruc = self.imageStruc[:n-1] + "0" + self.imageStruc[n:]
-        self.image = Image(self.imageStruc)
-        print("[INFO] STRUCTURE IMAGE", self.imageStruc)
 
 
 def get_id() -> int:
@@ -141,15 +88,15 @@ def get_id() -> int:
     Si elle reçoit la première le message d'une autre micro:bit, elle devient la m:b 2 et envoie une réponse autaurisant l'autre m:b à devenir la m:b 1.
     """
     display.show(Image("00000:""00000:""90909:""00000:""00000"))
-    radio.send("ID|1|1") # est ce que je peux etre la m:b 1?
+    send_message("IDa|1|1") # est ce que je peux etre la m:b 1?
     while True:
         message = radio.receive()
         if message:
-            if message == "ID|1|1": #est ce que je peux etre la m:b 1?
-                radio.send("ID|1|2") # oui, tu peux! je vais etre la m:b 2 alors!
+            if message == "IDa|1|1": #est ce que je peux etre la m:b 1?
+                send_message("IDc|1|2") # oui, tu peux! je vais etre la m:b 2 alors!
                 print("ID: 2")
                 return 2
-            elif message == "ID|1|2": # oui, tu peux! je vais etre la m:b 2 alors!
+            elif message == "IDc|1|2": # oui, tu peux! je vais etre la m:b 2 alors!
                 print("ID: 1")
                 return 1
 
@@ -173,21 +120,21 @@ def get_role() -> str:
     display.show("?")
     while True:
         message = radio.receive()
-        if button_a.is_pressed() or message == "ROLE|1|E":
-            radio.send("ROLE|1|P")
+        if button_a.is_pressed() or message == "ROLEc|1|E":
+            send_message("ROLEc|1|P")
             display.show("P")
             sleep(1000)
             display.clear()
             print("Parent")
             return "P"
-        elif button_b.is_pressed() or message == "ROLE|1|P":
-            radio.send("ROLE|1|E")
+        elif button_b.is_pressed() or message == "ROLEc|1|P":
+            send_message("ROLEc|1|E")
             display.show("E")
             sleep(1000)
             display.clear()
             print("Child")
             return "E"
-        
+       
 def wait_for_button_up_not_cenceled(button: str) -> bool:
     """
     prend en paramètre le bouton à attendre (a, b ou ab)
@@ -210,6 +157,7 @@ def wait_for_button_up_not_cenceled(button: str) -> bool:
     elif button.lower() == "ab":
         while button_a.is_pressed() or button_b.is_pressed():
             pass
+
 def check_mdp():
     imageMdp = ImageMdp()
     while True:
@@ -224,23 +172,29 @@ def check_mdp():
         if check:
             return True
 
-# initialisation de l'ID et du rôle
+def send_message(message: str) -> None:
+    """
+    Envoie un message à l'autre m:b 
+    (le but va etre de transformer cette fonction pour qu'elle soit encryptée plus tard comme ça on ne doit pas repasser partout dans le code)
+    """
+    radio.send(message)
 
+# initialisation de l'ID et du rôle
 if not DEV_BYPASS_GET_ID:
     ID = get_id()
 else:
     ID = 1
 
-
-# debug - - - - - - -
+# debug - - - - - - - (affiche l'ID de la m:b si il y en a deux)
 if not DEV_BYPASS_GET_ID:
     display.show(str(ID))
     sleep(1000)
     display.clear()
 
+# début du programme
+
 ROLE = get_role()
 
-# initialisation de la m:b Enfant
 if ROLE == "E":
     # initialisation de la m:b Enfant
     class Enfant:
@@ -250,12 +204,60 @@ if ROLE == "E":
 
     # tout le code de la m:b Enfant ici
     while True:
-        radio.send("STATUS|6|ASLEEP")
+        send_message("STATUSr|6|ASLEEP")
         sleep(1500)
 
-# initialisation de la m:b Parent
 elif ROLE == "P":
     # initialisation de la m:b Parent
+    IMAGE_SLEEP_SEQUENCE = [Image("00000:""00000:""99099:""00000:""09990"),Image("00000:""99990:""00900:""09000:""99990"), Image("09999:""00090:""00900:""09999:""00000")]
+    
+    class ImageCompteurLait():
+        """Classe permettant de gérer une représentation visuelle de la quantité de lait sur l'écran de la m:b Parent"""
+        def __init__(self) -> None:
+            self.imageStruc = "00000:""00000:""00000:""00000:""00000"
+            self.image = Image(self.imageStruc)
+
+        def showImage(self):
+            """Retourne l'image de la quantité de lait"""
+            return Image(self.imageStruc)
+
+        def checkStruc(self):
+            """Retourne l'index du premier 0 dans la structure de l'image"""
+            n = 0
+            for i in self.imageStruc:
+                if str(i).isdigit():
+                    if i == "0":
+                        return n
+                n+=1
+            return n
+        
+        def addPin(self) -> None:
+            """Ajoute un point à la représentation visuelle de la quantité de lait"""
+            n = self.checkStruc()
+            if n > 28:
+                pass
+            else:
+                print("OUI") # on peut retirer ?
+                self.imageStruc = self.imageStruc[:n] + "9" + self.imageStruc[n + 1:]
+            self.image = Image(self.imageStruc)
+            print("[INFO] STRUCTURE IMAGE", self.imageStruc)
+
+        def removePin(self) -> None:
+            """Retire un point à la représentation visuelle de la quantité de lait"""
+            n = self.checkStruc()
+            print(n)
+            if n > 28:
+                self.imageStruc = "99999:""99999:""99999:""99999:""99990"
+            elif n == 0:
+                pass
+            else:
+                if n in [6, 12, 18, 24]:
+                    self.imageStruc = self.imageStruc[:n-2] + "0:" + self.imageStruc[n:]
+                else:
+                    self.imageStruc = self.imageStruc[:n-1] + "0" + self.imageStruc[n:]
+            self.image = Image(self.imageStruc)
+            print("[INFO] STRUCTURE IMAGE", self.imageStruc)
+
     class Parent:
         """Classe contenant les methodes et attributs de la m:b Parent"""
         def __init__(self) -> None:
@@ -347,18 +349,16 @@ elif ROLE == "P":
                 if pin_logo.is_touched():
                     self.menu()
 
-
-
         def mode_status(self):
             """permet de voir l'état de l'Enfant"""
-            # NOT DEBUGGED YET
+
             animation_counter = 0
             display.show(IMAGE_SLEEP_SEQUENCE[1])
             while True:
                 if pin_logo.is_touched():
                     self.menu()
                 message = radio.receive()
-                if message == "STATUS|6|ASLEEP":
+                if message == "STATUSr|6|ASLEEP":
                     animation_counter += 1
                     if animation_counter % 5 == 0:
                         display.show(IMAGE_SLEEP_SEQUENCE[0])
@@ -371,15 +371,23 @@ elif ROLE == "P":
             """permet de trouver la m:b Enfant"""
             # A FAIRE (feature bonus)
             # l'idée va être de faire un jeu de chaud/froid en utilisant la force du signal radio (c'est possible)
+
             while not pin_logo.is_touched():
-                if radio.receive_full():
-                    force_signal = radio.receive_full()
+                force_signal = radio.receive_full()
+                if force_signal:
+                    force_signal = force_signal[1]
                     print(force_signal)
-                # if force_signal > -50:
-                #     display.show("3")
-                # elif force_signal > -100:
-                #     display.show("2")
-                # else:
-                #     display.show("1")
+                    if force_signal > -50:
+                        display.show("3")
+                    elif force_signal > -100:
+                        display.show("2")
+                    else:
+                        display.show("1")
             self.menu()
     Parent().menu()
+
+else:
+    display.show(Image.SAD)
+    sleep(1000)
+    # si le rôle n'est pas "P" ou "E", il y a une erreur de rôle
+    display.scroll("ROLE ERROR")
